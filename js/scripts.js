b04b0a378693aed795ca3cba1ff1cddfe3c75b12
michalgrochowski <mic.grochowski@gmail.com>
2017-01-10 21:15:10 +0100

    DobryWebdev
    v2.0

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(window).on('scroll', function(){
+        $('#bs-example-navbar-collapse-1').collapse('hide')
+    });
+});
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+})
+//-- Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-90010989-1', 'auto');
+ga('send', 'pageview');
